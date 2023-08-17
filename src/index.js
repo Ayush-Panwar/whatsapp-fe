30cc8af6fd7b3504c89abf1e697cff0e189c2dc1
Ayush-Panwar <ayushpanwar717@gmail.com>
2023-08-18 02:32:00 +0530

    WhatsApp clone group chat

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import * as process from "process";
 
+window.global = window;
+window.process = process;
+window.Buffer = [];
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
